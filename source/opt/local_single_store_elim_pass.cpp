6bb50a2f45e06d77dbd62205e4d898dcae5b7a56
MaxwellGeng <maxwellgeng@outlook.com>
2026-08-15 12:35:29 +0800

    Add SPV_NV_cooperative_vector to optimizer extension allow-lists

--- source/opt/local_single_store_elim_pass.cpp
+++ source/opt/local_single_store_elim_pass.cpp
@@ -128,6 +128,7 @@ void LocalSingleStoreElimPass::InitExtensionAllowList() {
       "SPV_NV_compute_shader_derivatives",
       "SPV_NV_shader_image_footprint",
       "SPV_NV_shading_rate",
+      "SPV_NV_cooperative_vector",
       "SPV_NV_mesh_shader",
       "SPV_EXT_mesh_shader",
       "SPV_NV_ray_tracing",
